e82369920066bfa0e5822085e4d7190892668464
abdulkarimyulchibekov <abdulkarimyulchibekov@gamil.com>
2022-08-12 18:06:53 +0500

    add changes

--- src/Components/PopularItem/PopularItem.jsx
+++ src/Components/PopularItem/PopularItem.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
-export const TopRatedItem = ({img, name, overview, point, date}) => {
+export const PopularItem = ({ img, name, overview, point, date }) => {
 	return (
 		<li className='toprated-item'>
 			<img src={`https://image.tmdb.org/t/p/w500/${img}`} />
 			<div className='toprated-item__body'>
 				<h2>{name}</h2>
-				<p>
-					{
-            overview
-          }
-				</p>
-				<span className='point'>
-          {
-            point
-          }
-        </span>
+				<p>{overview}</p>
+				<span className='point'>{point}</span>
 				<p>{date}</p>
 			</div>
 		</li>
--- src/Private.js
+++ src/Private.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import { Header } from './Components/Header/Header';
+import { Popular } from './Components/Popular/Popular';
 import { TopRated } from './Components/TopRated/TopRated';
 import { Home } from './Pages/Home/Home';
 import { Login } from './Pages/Login/Login';
@@ -12,8 +13,8 @@ export const Private = () => {
 			<Header />
 			<Routes>
 				<Route path='/' element={<Home />} />
-				<Route path='/popular/*' element={<h1>Popular movies</h1>} />
-				<Route path='/top-rated/*' element={<TopRated/>} />
+				<Route path='/popular/*' element={<Popular />} />
+				<Route path='/top-rated/*' element={<TopRated />} />
 				<Route path='/coming/*' element={<h1>Up coming Movies</h1>} />
 				<Route path='/tv-show/*' element={<h1>TV Show</h1>} />
 				<Route path='*' element={<>404 ERROR bunaqa page topilmadi!!!</>} />
